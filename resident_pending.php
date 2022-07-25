f70d74f099717fedb9677b83fe73b203c6bf5a5a
Grace Caballero <107015315+Gracecaballero@users.noreply.github.com>
2022-07-25 10:30:08 +0800

    added sql statement - first try dunno saktu ba 😁

--- resident_pending.php
+++ resident_pending.php
@@ -17,6 +17,10 @@
 	while($row = $result1->fetch_assoc()){
 		$purok[] = $row; 
 	}
+//     SELECT tbldocument_request.id AS No, tbldocument_request.request_date AS Date, tblresident.firstname + `, ` + tblresident.middlename + `, ` +  tblresident.lastname AS Name, tbldocument_request.request_service AS Services, tbldocument_request.request_purpose AS Purpose, tbldocument_request.request_payment_method AS PaymentMethod, tbldocument_request.request_ref_no AS ReferenceNumber, tbldocument_request.request_status AS Status
+// FROM tbldocument_request
+// RIGHT JOIN tblresident ON tbldocument_request.resident_id = tblresident.id
+// ORDER BY tbldocument_request.id;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,12 +43,6 @@
 			<div class="content">
 				<div class="panel-header bg-primary-gradient">
 					<div class="page-inner">
-						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-							<div>
-								<h2 class="text-white fw-bold">Requested Documents</h2>
-							</div>
-						</div>
-                        
 					</div>
 				</div>
 				<div class="page-inner">
@@ -61,7 +59,7 @@
                             <div class="card">
 								<div class="card-header">
 									<div class="card-head-row">
-										<div class="card-title">Resident Information</div>
+										<div class="card-title">Requested Documents</div>
                                         <?php if(isset($_SESSION['username'])):?>
 										<div class="card-tools">
                                             <a href="model/export_resident_csv.php" class="btn btn-danger btn-border btn-round btn-sm">
@@ -152,205 +150,7 @@
 					</div>
 				</div>
 			</div>
-            <!-- Modal -->
-            <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Edit/View Resident Information</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form method="POST" action="model/edit_resident.php" enctype="multipart/form-data">
-                            <input type="hidden" name="size" value="1000000">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div id="my_camera1" style="width: 370px; height: 250;" class="text-center">
-                                        <img src="assets/img/person.png" alt="..." class="img img-fluid" width="250" id="image">
-                                    </div>
-                                    <?php if(isset($_SESSION['username'])):?>
-                                    <div class="form-group d-flex justify-content-center">
-                                        <button type="button" class="btn btn-danger btn-sm mr-2" id="open_cam1">Open Camera</button>
-                                        <button type="button" class="btn btn-secondary btn-sm ml-2" onclick="save_photo1()">Capture</button>   
-                                    </div>
-                                    <div id="profileImage1">
-                                        <input type="hidden" name="profileimg">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="file" class="form-control" name="img" accept="image/*">
-                                    </div>
-                                    <?php endif ?>
-                                    <div class="form-group">
-                                        <div class="selectgroup selectgroup-secondary selectgroup-pills">
-                                            <label class="selectgroup-item">
-                                                <input type="radio" name="deceased" value="1" class="selectgroup-input" checked="" id="alive">
-                                                <span class="selectgroup-button selectgroup-button-icon"><i class="fa fa-walking"></i></span>
-                                            </label><p class="mt-1 mr-3"><b>Alive</b></p>
-                                            <label class="selectgroup-item">
-                                                <input type="radio" name="deceased" value="0" class="selectgroup-input" id="dead">
-                                                <span class="selectgroup-button selectgroup-button-icon"><i class="fa fa-people-carry"></i></span>
-                                            </label><p  class="mt-1 mr-3"><b>Deceased</b></p>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>National ID No.</label>
-                                        <input type="text" class="form-control" name="national" id="nat_id" placeholder="Enter National ID No.">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Citizenship</label>
-                                        <input type="text" class="form-control" name="citizenship" id="citizenship" placeholder="Enter citizenship" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Firstname</label>
-                                                <input type="text" class="form-control" placeholder="Enter Firstname" name="fname" id="fname" required>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Middlename</label>
-                                                <input type="text" class="form-control" placeholder="Enter Middlename" name="mname" id="mname" required>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Lastname</label>
-                                                <input type="text" class="form-control" placeholder="Enter Lastname" name="lname" id="lname" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Alias</label>
-                                                <input type="text" class="form-control" placeholder="Enter Alias" id="alias" name="alias">
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Place of Birth</label>
-                                                <input type="text" class="form-control" placeholder="Enter Birthplace" name="bplace" id="bplace" required>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Birthdate</label>
-                                                <input type="date" class="form-control" placeholder="Enter Birthdate" name="bdate" id="bdate" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Age</label>
-                                                <input type="number" class="form-control" placeholder="Enter Age" min="1" name="age" id="age" required>
-                                                </div>
-                                            </div>
-                                            <div class="col">
-                                                <div class="form-group">
-                                                <label>Civil Status</label>
-                                                <select class="form-control" required name="cstatus" id="cstatus">
-                                                    <option disabled selected>Select Civil Status</option>
-                                                    <option value="Single">Single</option>
-                                                    <option value="Married">Married</option>
-                                                    <option value="Widow">Widow</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Gender</label>
-                                                <select class="form-control" required name="gender" id="gender">
-                                                    <option disabled selected value="">Select Gender</option>
-                                                    <option value="Male">Male</option>
-                                                    <option value="Female">Female</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Purok</label>
-                                                <select class="form-control" required name="purok" id="purok">
-                                                    <option disabled selected>Select Purok Name</option>
-                                                    <?php foreach($purok as $row):?>
-                                                        <option value="<?= ucwords($row['name']) ?>"><?= $row['name'] ?></option>
-                                                    <?php endforeach ?>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Voters Status</label>
-                                                <select class="form-control vstatus" required name="vstatus" id="vstatus">
-                                                    <option disabled selected>Select Voters Status</option>
-                                                    <option value="Yes">Yes</option>
-                                                    <option value="No">No</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Identified As</label>
-                                                <select class="form-control indetity" name="indetity" id="indetity">
-                                                    <option disabled selected>Select Identity</option>
-                                                    <option value="Positive">Positive</option>
-                                                    <option value="Negative">Negative</option>
-                                                    <option value="Unidentified">Unidentified</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Email Address</label>
-                                                <input type="email" class="form-control" placeholder="Enter Email" name="email" id="email">
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Contact Number</label>
-                                                <input type="text" class="form-control" placeholder="Enter Contact Number" name="number" id="number">
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label>Occupation</label>
-                                                <input type="text" class="form-control" placeholder="Enter Occupation" name="occupation" id="occupation">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Address</label>
-                                        <textarea class="form-control" required name="address" placeholder="Enter Address" id="address"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Remarks</label>
-                                        <textarea class="form-control" name="remarks" placeholder="Enter remarks" id="remarks"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <input type="hidden" name="id" id="res_id">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <?php if(isset($_SESSION['username'])): ?>
-                            <button type="submit" class="btn btn-primary">Update</button>
-                            <button type="submit" class="btn btn-success">Approve</button>
-                            <?php endif ?>
-                        </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            
 
 			<!-- Main Footer -->
 			<?php include 'templates/main-footer.php' ?>
