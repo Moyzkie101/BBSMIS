40bc491c25a11ed3ab19c76d0d1f5fcd27c39a16
Moyzkie101 <102865569+Moyzkie101@users.noreply.github.com>
2022-07-25 19:34:29 +0800

    Added to customized LOGIN FORM w/ files & folders

---
+++ pages/logins/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN ACCOUNT</title>
+</head>
+<body>
+    <div>
+        <form>
+            <div>
+                <h3>
+                    <strong>
+                        LOGIN FORM
+                    </strong>
+                </h3>
+            </div>
+            <div>
+                <label><b>Username</b></br></label>
+                <input type="text" placeholder="Enter Username"></br>
+            </div>
+            <div>
+                <label><b>Password</b></br></label>
+                <input type="text" placeholder="Enter Password"></br>
+            </div>
+            <button>Login</button>
+            <button>Create Account</button>
+        </form>
+    </div>
+</body>
+</html>
